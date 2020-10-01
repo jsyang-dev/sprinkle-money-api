5c1cf5ee694e15aa1e19f082cef8d1cdcfe0f3d2
Jeongsu Yang <mycat83@gmail.com>
2020-10-01 17:52:57 +0900

    기능: DB 모델링
    * JPA 엔티티 2개 생성
    
    관련: #2

---
+++ src/main/java/com/kakaopay/domain/Receiving.java
@@ -0,0 +1,36 @@
+package com.kakaopay.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Receiving extends BaseEntity {
+
+  // 받은 금액
+  @Column(nullable = false)
+  private Long amount;
+
+  // 받은 사용자
+  @Column private Integer userId;
+
+  // Lock 버전
+  @Version private Integer version;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sprinkling_id")
+  private Sprinkling sprinkling;
+}
